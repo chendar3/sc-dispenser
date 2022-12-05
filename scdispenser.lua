9d065fd4ec0f595536355547b4bbea1782891ae4
chendar3 <116913203+chendar3@users.noreply.github.com>
2022-12-05 17:39:33 +0100

    Update scdispenser.lua

--- scdispenser.lua
+++ scdispenser.lua
@@ -30,12 +30,14 @@ _addon.name = 'Skillchain Dispenser'
 _addon.author = 'Chendar'
 _addon.version = '1.0'
 _addon.command = 'scd'
-_addon.commands = {'sc', '3step', '6step', 'cycle'}
+_addon.commands = {'sc', 'element', 'burst', 'ebullience'}
 
 require('tables')
+texts = require('texts')
 res = require('resources')
 
 function init()
+	check_job()
 	Elements= T{'Fire','Water','Thunder','Stone','Aero','Blizzard','Light','Dark'}
 	Ele_index = 1
 	Skillchain = {}
@@ -95,15 +97,90 @@ function init()
 			closer="Noctohelix",
 			delay=4,
 			}
+	
+	Colors = T{}
+		Colors.Fire = {204, 0, 0} 
+		Colors.Water = {0, 102, 204}
+		Colors.Aero = {51, 102, 0}
+		Colors.Light = {255, 255, 255}
+		Colors.Stone = {139, 139, 19}
+		Colors.Blizzard = {0, 204, 204}
+		Colors.Thunder = {102, 0, 204}
+		Colors.Dark = {0, 0, 0}
+					
+	Burst = T{'off', 'on', 'helix'}
+	Burst_index = 1
+	Ebullience = false	
+
+	BurstMode = Burst[Burst_index]
+	CurrentElement = Elements[Ele_index]	
+	Color = Colors[CurrentElement]
+			
+	HUD = texts.new()
+	HUD:text('Element: '..CurrentElement..'\n'..'Burst: '..BurstMode..'\n'..'Ebullience: '..tostring(Ebullience))
+	HUD:font('Impact')
+	HUD:size(12)
+	HUD:bg_alpha(200)	
+	HUD:bg_color(40, 40, 55)
+	HUD:pos(15,90)
+	HUD:color(Color[1], Color[2], Color[3])
+	HUD:show()
 end
 
-function make_boom()
+function make_boom(arg)
 end
 
 function check_job()
 end
 
-function commands(cmd, arg)
+function update_element()
+	CurrentElement = Elements[Ele_index]
+	Color = Colors[CurrentElement]
+end
+
+function update_hud()
+	HUD:text('Element: '..CurrentElement..'\n'..'Burst: '..BurstMode..'\n'..'Ebullience: '..tostring(Ebullience))
+	HUD:color(Color[1], Color[2], Color[3])
+end
+
+handle_commands = function(...)
+	local args = T{...}		
+    if args ~= nil then
+        local cmd = table.remove(args,1):lower()
+		if cmd == 'sc' then
+			make_boom(args)
+		elseif cmd == 'element' then
+			if args[1] ~= nil and Elements:contains(args[1]) then
+				Ele_index = table.find(Elements, args[1])
+				update_element()				
+				--CurrentElement = args[1]
+				--Color = Colors[CurrentElement]
+				update_hud()
+			else
+				Ele_index = Ele_index + 1 
+				if Ele_index > #Elements then Ele_index = 1 end
+				update_element()
+				update_hud()
+			end
+		elseif cmd == 'burst' then
+			if args[1] ~= nil and Burst:contains(args[1]) then
+				Burst = args[1]
+				update_hud()
+			else
+				Burst_index = Burst_index + 1
+				if Burst_index > #Burst then Burst_index = 1 end
+				BurstMode = Burst[Burst_index]
+				update_hud()
+			end
+		elseif cmd == 'ebullience' then
+			if Ebullience then
+				Ebullience = false
+			else
+				Ebullience = true
+			end
+			update_hud()
+		end
+	end
 end
 
 function translate(term)
@@ -115,6 +192,6 @@ function translate(term)
 	return cache[term]
 end
 
-windower.register_event('addon command', commands(cmd, arg))
+windower.register_event('addon command', handle_commands)
 
 windower.register_event('load', init())
